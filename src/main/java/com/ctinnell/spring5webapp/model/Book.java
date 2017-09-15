ea20a9d4d1056224a05daa5787a363ab38ec5f2e
Clay Tinnell <ctinnell@gaig.com>
2017-09-15 13:00:03 -0400

    initialized test data

--- src/main/java/com/ctinnell/spring5webapp/model/Book.java
+++ src/main/java/com/ctinnell/spring5webapp/model/Book.java
@@ -20,6 +20,22 @@ public class Book {
         inverseJoinColumns = @JoinColumn(name = "author_id"))
     private Set<Author> authors = new HashSet<>();
 
+    public Book() {
+    }
+
+    public Book(String title, String isbn, String publisher) {
+        this.title = title;
+        this.isbn = isbn;
+        this.publisher = publisher;
+    }
+
+    public Book(String title, String isbn, String publisher, Set<Author> authors) {
+        this.title = title;
+        this.isbn = isbn;
+        this.publisher = publisher;
+        this.authors = authors;
+    }
+
     public Long getId() {
         return id;
     }
